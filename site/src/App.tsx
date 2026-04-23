c777ce4cdf7cf2c9e9ef822af3248582873d1bf2
Wang  Markus Imanuel Knudsen <markuswang234@gmail.com>
2026-04-24 00:25:23 +0200

    fix: reset scroll on page navigation

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,12 +1,24 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import SiteLayout from "./components/SiteLayout";
 import AboutPage from "./pages/AboutPage";
 import GuidePage from "./pages/GuidePage";
 import HomePage from "./pages/HomePage";
 
+function ScrollToTop() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [location.pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <SiteLayout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/guide" element={<GuidePage />} />
